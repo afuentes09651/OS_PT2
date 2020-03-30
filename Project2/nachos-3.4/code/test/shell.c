41bd17e6c73714cfdabf5cd4ad06aa0d3ec8776b
Alec <C00274244@louisiana.edu>
2020-03-30 13:29:22 -0500

    New Shell
    shell != aids

--- Project2/nachos-3.4/code/test/shell.c
+++ Project2/nachos-3.4/code/test/shell.c
@@ -1,35 +1,19 @@
 #include "syscall.h"
 
-int
-main()
-{
+int main(){
     SpaceId newProc;
-    OpenFileId input = ConsoleInput;
-    OpenFileId output = ConsoleOutput;
-    char prompt[2], ch, buffer[60];
-    int i;
-
-    prompt[0] = '-';
-    prompt[1] = '-';
-
-    while( 1 )
-    {
-	Write(prompt, 2, output);
-
-	i = 0;
-	
-	do {
-	
-	    Read(&buffer[i], 1, input); 
-
-	} while( buffer[i++] != '\n' );
-
-	buffer[--i] = '\0';
-
-	if( i > 0 ) {
-		newProc = Exec(buffer);
-		Join(newProc);
-	}
+    char *prompt = "student@NachOS > ", buffer[60];
+    while( 1 ){
+	    Write(prompt, 32, ConsoleOutput);   // Write our prompt string to console 
+        Read(buffer, 60, ConsoleInput);     // Get user input
+        // quit the shell if user enters "exit"
+        if(buffer[0]=='e'&&buffer[1]=='x'&&buffer[2]=='i'&&buffer[3]=='t'&&buffer[4]=='\0'){
+            Exit(0);
+        }
+        newProc = Exec(buffer);             // Exec examples: "../test/shell" "../test/sort" etc...
+        if(newProc >= 0){                   // This assumes that SpaceId < 0 represents an error 
+            Join(newProc);
+        }
     }
 }
 
